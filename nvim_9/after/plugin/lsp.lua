20f12f795d51c4a6c52d4129630b1cf17a22ca92
Known Black Hat <65008967+KnownBlackHat@users.noreply.github.com>
2023-08-11 03:07:51 -0400

    perf: update lsp-zero

--- nvim_9/after/plugin/lsp.lua
+++ nvim_9/after/plugin/lsp.lua
@@ -1,36 +1,45 @@
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = false,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+
+lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
 })
 
 local cmp = require('cmp')
-
-lsp.setup_nvim_cmp({
-  mapping = lsp.defaults.cmp_mappings({
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = vim.NIL,
-    ['<S-Tab>'] = vim.NIL,
-    ['<CR>'] = vim.NIL,
-  })
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y'] = cmp.mapping.confirm(cmp_select),
+    ['<C-Space'] = cmp.mapping.complete(),
 })
 
-cmp.setup({   window = {
+lsp.set_preferences({
+    sign_icons = { }
+})
+
+cmp.setup({
+    window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     }})
 
 lsp.on_attach(function (client, bufnr)
     local opts = {buffer = bufnr, remap = false}
-    vim.keymap.set("n","<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-end)
--- When you don't have mason.nvim installed
--- You'll need to list the servers installed in your system
-lsp.setup_servers({'tsserver', 'eslint'})
 
--- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
+    vim.keymap.set("n","gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n","K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n","<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n","<leader>vd", function() vim.lsp.buf.open_float() end, opts)
+    vim.keymap.set("n","[d", function() vim.lsp.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n","]d", function() vim.lsp.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n","<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n","<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n","<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i","<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
 
 lsp.setup()
-
