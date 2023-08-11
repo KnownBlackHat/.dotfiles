f46700fef8603e2bd8983b59bdcf4bf0be8ac13e
Known Black Hat <65008967+KnownBlackHat@users.noreply.github.com>
2023-08-11 13:11:26 -0400

    ui: add sign column color

--- nvim_9/after/plugin/colors.lua
+++ nvim_9/after/plugin/colors.lua
@@ -10,7 +10,9 @@ function ColorMyPencils(color)
     vim.api.nvim_set_hl(0,"LineNr", { fg = "LightGrey" })
     vim.api.nvim_set_hl(0,"Identifier", { fg = "#29fefe" })
     vim.api.nvim_set_hl(0,"Statement", { fg = "yellow" })
-    vim.api.nvim_set_hl(0,"Type", { fg = "#00ff00" })
+    vim.api.nvim_set_hl(0,"Type", { fg = "#00ff00" })    
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg='#1f1e1e' })
+
 end
 
 ColorMyPencils()
