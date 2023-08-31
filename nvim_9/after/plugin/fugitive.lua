4a4affd166931fd7ccbfba1e08aac25e45d2ed8f
Known Black Hat <65008967+KnownBlackHat@users.noreply.github.com>
2023-08-31 05:24:29 -0400

    fix: remove mapping for sign commit

--- nvim_9/after/plugin/fugitive.lua
+++ nvim_9/after/plugin/fugitive.lua
@@ -1,4 +1,3 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
 vim.keymap.set("n", "gr", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "gu", "<cmd>diffget //3<CR>")
-vim.keymap.set("n", "ci", "<cmd>Git commit -S<CR>")
