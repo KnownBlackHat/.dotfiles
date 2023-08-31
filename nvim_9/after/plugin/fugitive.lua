bab84ea83619d51e86ec7695558bce3ded4f40bf
Known Black Hat <65008967+KnownBlackHat@users.noreply.github.com>
2023-08-31 01:49:37 -0400

    feat: add keymap for signed commit

--- nvim_9/after/plugin/fugitive.lua
+++ nvim_9/after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
+vim.keymap.set("n", "gr", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gu", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "ci", "<cmd>Git commit -S<CR>")
