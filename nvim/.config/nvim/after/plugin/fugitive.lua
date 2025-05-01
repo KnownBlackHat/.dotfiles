ce42ba3de7b8a5496ad937b071df3ef035980815
KnownBlackHat <knownblackhat@gmail.com>
2025-05-01 10:45:37 -0400

    feat: add dual screen polybar support

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,3 +1,3 @@
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
-vim.keymap.set("n", "gr", "<cmd>diffget //2<CR>")
-vim.keymap.set("n", "gu", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gr", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "<leader>gu", "<cmd>diffget //3<CR>")
