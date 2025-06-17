9e737d59cc5828c809713985be82e6aa31e6b3a8
KnownBlackHat <knownblackhat@gmail.com>
2025-06-17 04:48:38 -0400

    feat: add dart support

--- nvim/.config/nvim/lua/knownblackhat/packer.lua
+++ nvim/.config/nvim/lua/knownblackhat/packer.lua
@@ -1,51 +1,60 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    use({
-        "stevearc/conform.nvim",
-        config = function()
-            require("conform").setup()
-        end,
-    })
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup()
+		end,
+	})
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('nvim-treesitter/playground')
-    use('mbbill/undotree')
-    use('tpope/vim-commentary')
-    use('github/copilot.vim')
-    use('vim-airline/vim-airline-themes')
-    use('vim-airline/vim-airline')
-    -- use('mattn/emmet-vim')
-    use('tpope/vim-fugitive')
-    use ('nvim-treesitter/nvim-treesitter-context')
-    use('terryma/vim-multiple-cursors')
-    use('lukas-reineke/indent-blankline.nvim')
-    use('dstein64/vim-startuptime')
-    use {
-        'VonHeikemen/lsp-zero.nvim', branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("mbbill/undotree")
+	use("tpope/vim-commentary")
+	use("github/copilot.vim")
+	use("vim-airline/vim-airline-themes")
+	use("vim-airline/vim-airline")
+	-- use('mattn/emmet-vim')
+	use("tpope/vim-fugitive")
+	use("nvim-treesitter/nvim-treesitter-context")
+	use("terryma/vim-multiple-cursors")
+	use("lukas-reineke/indent-blankline.nvim")
+	use("dstein64/vim-startuptime")
+	use({
+		"akinsho/flutter-tools.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+	})
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
-        }
-    }
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
+		},
+	})
 end)
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -289,3 +289,8 @@ export PYENV_ROOT="$HOME/.pyenv"
 # Enable vim mode
 # set -o vi
 export MANPAGER='nvim +Man!'
+export ANDROID_HOME="$HOME/Downloads/flutter-install-files/cmdline-tools"
+export PATH="$PATH:$HOME/Downloads/flutter-install-files/cmdline-tools/bin:$HOME/Downloads/flutter-install-files/flutter/bin:$HOME/Downloads/flutter-install-files/cmdline-tools"
+export ANDROID_SDK_ROOT=$HOME/Downloads/flutter-install-files
+export PATH=$PATH:$ANDROID_SDK_ROOT/cmdline-tools/bin
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
