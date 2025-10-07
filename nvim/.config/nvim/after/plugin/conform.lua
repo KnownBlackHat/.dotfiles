5d3646d725197a4a3f478aec1b783cc7ef81aabe
KnownBlackHat <knownblackhat@gmail.com>
2025-10-07 12:54:29 -0400

    feat: some compatibility changes

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -1,22 +1,22 @@
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-    },
-    notify_on_error = false,
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 5000,
-        lsp_fallback = true,
-    }
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { { "prettier" } },
+	},
+	notify_on_error = false,
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 5000,
+		lsp_fallback = true,
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-    end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
--- nvim/.config/nvim/lua/knownblackhat/mapping.lua
+++ nvim/.config/nvim/lua/knownblackhat/mapping.lua
@@ -8,11 +8,11 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- selected text gets deleted and goes to _ (black hole) register and the do before paste
-vim.keymap.set("v", "p", "\"_dP")
+vim.keymap.set("v", "p", '"_dP')
 
-vim.keymap.set("n", "y", "\"+y")
-vim.keymap.set("v", "y", "\"+y")
-vim.keymap.set("n", "Y", "\"+Y")
+vim.keymap.set("n", "y", '"+y')
+vim.keymap.set("v", "y", '"+y')
+vim.keymap.set("n", "Y", '"+Y')
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Disabling arrow key
@@ -25,19 +25,19 @@ vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlighting when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlighting when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader><Tab>", vim.lsp.buf.hover)
 
-vim.g.multi_cursor_quit_key = '<Tab>'
+vim.g.multi_cursor_quit_key = "<Tab>"
 
-vim.g.user_emmet_leader_key = ','
+vim.g.user_emmet_leader_key = ","
 
 vim.api.nvim_set_keymap("i", "<C-K>", "copilot#Accept('<CR>')", { expr = true, silent = true })
